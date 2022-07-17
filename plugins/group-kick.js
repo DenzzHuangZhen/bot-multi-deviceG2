15c84573786b2900ef01e0d3c32c92425eef69ac
AyGemuy <91945119+AyGemuy@users.noreply.github.com>
2022-07-17 13:19:26 +0800

    New
    New

--- plugins/group-kick.js
+++ plugins/group-kick.js
@@ -13,7 +13,7 @@ let handler = async (m, { conn, participants }) => {
 }
 handler.help = ['kick', '-'].map(v => 'o' + v + ' @user')
 handler.tags = ['owner']
-handler.command = /^(okick|o-)$/i
+handler.command = /^(gkick|g-)$/i
 
 handler.owner = true
 handler.group = true
---
+++ plugins/user-kick.js
@@ -0,0 +1,20 @@
+let handler = async (m, { conn, args }) => {
+import fs from 'fs'
+ let ownerGroup = m.chat.split`-`[0] + '@s.whatsapp.net'
+  aki = m.quoted ? [m.quoted.sender] : m.mentionedJid
+  let users = aki.filter(u => !(u == ownerGroup || u.includes(conn.user.jid)))
+  wayy = '_*Asik Dapet Jatah Kick*_'
+  for (let i of users) {
+  wayy += ` @${i.split('@')[0]}`
+  }
+  conn.reply(m.chat, wayy, m, { contextInfo: { mentionedJid: users }})
+  for (let user of users) if (user.endsWith('@s.whatsapp.net')) await conn.groupParticipantsUpdate(m.chat, [user], "remove")
+}
+handler.help = ['kick'].map(v => v + ' @user')
+handler.tags = ['group']
+handler.command = /^(kick|\-)$/i
+handler.owner = true
+handler.group = true
+handler.botAdmin = true
+
+export default handler
