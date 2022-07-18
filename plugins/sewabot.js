46081c2a4d42e47afd90bc5495941b528eb86417
KahfiBotz <109428066+KahfiBotz@users.noreply.github.com>
2022-07-18 19:46:38 +0700

    Update sewabot.js

--- plugins/sewabot.js
+++ plugins/sewabot.js
@@ -43,14 +43,14 @@ wa.me/${owner[0]}
   products:[{productId: '7487741964584083'}]}],
   headerImage: { productId: '7487741964584083',
   jpegThumbnail: baper },
-  businessOwnerJid: nomorown + '@s.whatsapp.net'
+  businessOwnerJid: nomorown + '6285380166282@s.whatsapp.net'
   },
   footerText: 'https://s.id/Cerdasin62',
   }},{quoted: ftroli})
   await conn.relayMessage(prepare.key.remoteJid,prepare.message,{messageId:prepare.key.id})
   
   // Kontak
-  let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:;${author};;;\nFN:${author}\nORG:${author}\nTITLE:\nitem1.TEL;waid=6282195322106:+62 821-9532-2106\nitem1.X-ABLabel:${author}\nX-WA-BIZ-DESCRIPTION:${htjava} Nih owner ku kack yg mengaktifkan aq.\nX-WA-BIZ-NAME:${author}\nEND:VCARD`
+  let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:;${author};;;\nFN:${author}\nORG:${author}\nTITLE:\nitem1.TEL;waid=6285380166282:+62 853-8016-6282\nitem1.X-ABLabel:${author}\nX-WA-BIZ-DESCRIPTION:${htjava} Nih owner ku kack yg mengaktifkan aq.\nX-WA-BIZ-NAME:${author}\nEND:VCARD`
 await conn.sendMessage(m.chat, { contacts: { displayName: wm, contacts: [{ vcard }] }}, {quoted: m})
 }
 
